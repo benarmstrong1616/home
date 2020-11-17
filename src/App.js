e90c03883363f03561de8b3d12098524881edbe1
benarmstrong1616 <ben.armstrong@live.co.uk>
2020-11-17 22:41:22 +0000

    sorting favicon

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import 'fontsource-roboto';
-import RecipeReviewCard from '/RecipeCard.js';
+import RecipeReviewCard from './RecipeCard.js';
 
 function App() {
   return (
