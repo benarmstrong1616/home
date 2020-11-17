3cc80ce461db2deec044612579f66d4bf330f5b8
benarmstrong1616 <ben.armstrong@live.co.uk>
2020-11-17 19:54:39 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import 'fontsource-roboto';
+import RecipeReviewCard from '/RecipeCard.js';
+
+function App() {
+  return (
+    <div className="App">
+      <RecipeReviewCard/>
+    </div>
+  );
+}
+
+export default App;
