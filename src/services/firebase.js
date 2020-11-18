e31f92229e23f79cc221204c505f5ecc3143865a
benarmstrong1616 <ben.armstrong@live.co.uk>
2020-11-18 22:42:47 +0000

    chat app

---
+++ src/services/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+const firebaseConfig = {
+    apiKey: "AIzaSyDs6-NrDB9GpjBswHpl46FfjWUOl3ASomY",
+    authDomain: "armstrongsoftwarehome.firebaseapp.com",
+    databaseURL: "https://armstrongsoftwarehome.firebaseio.com",
+    projectId: "armstrongsoftwarehome",
+    storageBucket: "armstrongsoftwarehome.appspot.com",
+    messagingSenderId: "158641605151",
+    appId: "1:158641605151:web:a1b161b7146108f418e32a",
+    measurementId: "G-N95J0HND6M"
+  };
+firebase.initializeApp(firebaseConfig);
+export const auth = firebase.auth;
+export const db = firebase.database();
